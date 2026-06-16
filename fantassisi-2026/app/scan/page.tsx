1c8403078e9be58dfe43a3da4d78b5fabfe57a5d
MaurizioBrasini <m.brasini@unimarconi.it>
2026-06-16 18:13:41 +0200

    Delete fantassisi-2026 directory

--- fantassisi-2026/app/scan/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client"; 
-import { Html5QrcodeScanner } from "html5-qrcode"; 
-export default function ScanPage() { 
-  new Html5QrcodeScanner("reader", { fps: 10 }).render(() =
-  return <div id="reader"></div>; 
-} 
