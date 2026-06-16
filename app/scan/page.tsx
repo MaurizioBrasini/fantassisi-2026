1909c560b7d8d4bac781006be8222df9f111eeec
MaurizioBrasini <m.brasini@unimarconi.it>
2026-06-16 18:22:54 +0200

    Update page.tsx

--- app/scan/page.tsx
+++ app/scan/page.tsx
@@ -1,6 +1,9 @@
-"use client"; 
-import { Html5QrcodeScanner } from "html5-qrcode"; 
-export default function ScanPage() { 
-  new Html5QrcodeScanner("reader", { fps: 10 }).render(() =
-  return <div id="reader"></div>; 
-} 
+"use client";
+
+import { Html5QrcodeScanner } from "html5-qrcode";
+
+export default function ScanPage() {
+  const scanner = new Html5QrcodeScanner("reader", { fps: 10 });
+  scanner.render(() => {});
+  return <div id="reader"></div>;
+}
